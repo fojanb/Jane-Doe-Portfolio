b289aeae2202cc926bbf0c39a7be6a15cd24a177
Fojan Babaali <fozhanmail@gmail.com>
2021-09-25 21:50:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-link");
-navToggle.addEventListener("click",()=>{
-    document.body.classList.toggle("nav-open");
-})
-navLinks.forEach(link => {
-    link.addEventListener("click",()=>{
-        document.body.classList.remove("nav-open");
-        window.scrollTo({
-            top: 100,
-            left: 0,
-            behavior: 'smooth'
-          });
-        
-
-    })
-})
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
